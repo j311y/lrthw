e6712d437bc73dc0d1ccc7fe7a631aa064d2ce6b
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-03-20 12:41:21 +0000

    Completed ex07

---
+++ ex7/ex7.rb
@@ -0,0 +1,20 @@
+puts "Mary had a little lamb." # puts string
+puts "It's fleece was white as #{'snow'}." # puts string with default value of 'snow'
+puts "And everywhere that Mary went." # puts string
+puts "." * 10 # puts string of '.' 10 times
+
+end1 = "C" # local var assignments
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6 # prints output with no line break after
+puts end7 + end8 + end9 + end10 + end11 + end12 # puts onto same line as print
