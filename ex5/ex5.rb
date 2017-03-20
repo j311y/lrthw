9eec1a31747a6945471e98c384893a737becc9d7
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-03-20 12:07:36 +0000

    Completed ex05

---
+++ ex5/ex5.rb
@@ -0,0 +1,19 @@
+name = "Jonathan J Elliot"
+age = 28
+height = 178 #cm
+weight = 77 #kg
+eyes = "Brown"
+teeth = "White"
+hair = "Brown"
+
+cm_to_inch = height / 2.54
+kgs_to_lbs = weight * 2.20462
+
+puts "Let's talk about #{name}."
+puts "He's #{height} cm tall."
+puts "He weighs #{weight} kg."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
