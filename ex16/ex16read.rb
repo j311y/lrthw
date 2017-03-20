83ef2233400d83ce5b66a7c119a86fc0e1f47fb4
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-03-20 18:06:30 +0000

    Completed ex16

---
+++ ex16/ex16read.rb
@@ -0,0 +1,13 @@
+filename = ARGV.first
+
+puts "We're going to read #{filename}"
+puts "If you don't want that, hit CTRL-C (^C)."
+puts "If you do want that, hit RETURN."
+
+$stdin.gets
+
+puts "Opening the file..."
+target = open(filename) {|f| data = f.read}
+
+puts "File closed."
+target.close
