6df6f6c474d3adec9621e3b4ddbb90a76a1fa44f
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-03-23 10:16:32 +0000

    Completed ex34

---
+++ ex34/ex34.rb
@@ -0,0 +1,8 @@
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+animals = ["tadpole", "dog", "mouse", "rabbit", "fish", "hen"]
+names = [["Jon", "Elliot"], ["Anna", "Bury"]]
+
+
+puts animals[5]
+puts array[2]
+puts names[1][0]
