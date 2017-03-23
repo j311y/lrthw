a7dba7722ac7907f162a89d8a165c1f7cd2f8b28
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-03-23 14:54:17 +0000

    Completed ex38

---
+++ ex38/ex38.rb
@@ -0,0 +1,32 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+
+puts "Wait there are not 10 things in that list. Let's fix that."
+
+stuff = ten_things.split(' ')
+more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while stuff.length != 10
+	next_one = more_stuff.pop
+	puts "Adding: #{next_one}"
+	stuff.push(next_one)
+	puts "There are #{stuff.length} items now."
+end
+
+# if stuff.length != 10
+# 	stuff.each do
+# 	next_one = more_stuff.pop
+# 	puts "Adding: #{next_one}"
+# 	stuff.push(next_one)
+# 	puts "There are #{stuff.length} items now."
+#     end
+# end
+
+puts "There we go: #{stuff}"
+
+puts "Lets do some things with stuff."
+
+puts stuff[1]
+puts stuff[-1]
+puts stuff.pop()
+puts stuff.join(' ')
+puts stuff[3..5].join("#")
